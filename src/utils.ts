4c7bf9c7d05939fe9cf3fe744eb9df23ef62f7c5
James Clark <jamesloganclark@gmail.com>
2020-10-08 16:30:46 -0700

    fix again

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,7 @@ import {
 
 export function isMobile(): boolean {
   return (
-    navigator &&
+    typeof window !== undefined &&
     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
       navigator.userAgent
     )
